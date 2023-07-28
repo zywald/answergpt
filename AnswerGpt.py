82f94cef2ee50b74b454b69691deaf782a2587c2
zywald <133850148+zywald@users.noreply.github.com>
2023-07-29 00:42:07 +0200

    Update AnswerGpt.py

--- AnswerGpt.py
+++ AnswerGpt.py
@@ -2,75 +2,108 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from openai.error import OpenAIError
+import time
 
+# Set up the title of the web application
 st.title("🦜🔗  - AnswerGPT")
 
+# Add a brief user guide
+st.sidebar.markdown("""
+### User Guide
+1. **Enter the original email that you want to reply to.**
+2. **Enter key points that should be included in the email response.**
+3. **Select the tone of the email response.**
+4. **Adjust the synthetic level of the response.**
+5. **Click "Submit" to generate the email response.** You can copy and modify the response in the "Email Response" box.
+""")
+
+# Add a slider for the user to select the synthetic level
+synthetic_level = st.sidebar.slider(
+    "Select the synthetic level:", min_value=0, max_value=6)
+
+# Get the OpenAI API key from the user
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-# Selection between two types of messages
-message_type = st.sidebar.selectbox(
-    "Select the type of message:", options=['Chat', 'Email'])
-
-# Selection between 4 tones
-tone = st.sidebar.selectbox("Select the tone of the message:", options=[
-                            'Professional', 'Friendly', 'Casual', 'Formal'])
-
-# Selection between two models
-model_name = st.sidebar.selectbox("Select the model:", options=['gpt-3.5-turbo', 'gpt-4'],
-                                  help="Choose 'gpt-3.5-turbo' for fast answer, or 'gpt-4' for more complex messages.")
+# Get the tone of the message from the user
+tone = st.sidebar.selectbox(
+    "Select the tone of the message:", options=['Casual', 'Formal'])
 
 
-def answer_message(original_message, message_to_reformulate):
+def define_instructions(original_message, key_points, synthetic_level, tone):
+    # Define the common instructions for the language model
+    common_instructions = (
+        "You are an AI assistant with expertise in crafting email responses. "
+        "Your responses should be clear, utilizing proper structured techniques like bullet points, and paragraph breaks where needed. "
+    )
+
+    synthetic_level_instructions = {
+        0: "Use complex sentence structures, longer sentences, and more formal language. ",
+        1: "Make the response relatively detailed. ",
+        2: "Make the response somewhat detailed, but still fairly concise. ",
+        3: "Balance conciseness and detail in the response. ",
+        4: "Make the response somewhat concise, but with some detail. ",
+        5: "Make the response relatively concise. ",
+        6: "Make the response very concise and straightforward. "
+    }
+
+    # Define the specific instructions based on user input
+    specific_instructions = synthetic_level_instructions[synthetic_level]
+    specific_instructions += f"Maintain a {tone} tone. "
+
+    if original_message:
+        specific_instructions += f"You are replying to the following email: '{original_message}'. "
+    if key_points:
+        specific_instructions += f"Ensure to include these key points in your response: '{key_points}'. "
+    else:
+        specific_instructions += "Provide a potential reply."
+    return common_instructions + specific_instructions
+
+
+def answer_message(original_message, key_points):
     try:
-        # Instantiate LLM model
-        llm = OpenAI(model_name=model_name, openai_api_key=openai_api_key)
+        # Initialize the language model
+        llm = OpenAI(model_name="gpt-4", openai_api_key=openai_api_key)
 
-        # Define common instructions
-        common_instructions = (
-            "You are an expert assistant trained to write clear, concise, and well-structured messages. "
-            "For email messages, use formatting techniques like bolding, italics, bullet points, and paragraph breaks where appropriate. "
-            "Your tone should be a balanced mix of conversational and Spartan efficiency. "
-            "For chat messages, do not sign the sender's name at the end. "
-            "You will answer in the email to reply and the draft email or indication language. "
-        )
+        # Combine the common and specific instructions
+        template = define_instructions(
+            original_message, key_points, synthetic_level, tone)
 
-        # Define specific instructions based on user input
-        specific_instructions = f"Write a {{message_type}} message in a {{tone}} tone. "
-        if original_message:
-            specific_instructions += f"The original message to reply to is: '{original_message}'. "
-        if message_to_reformulate:
-            specific_instructions += f"Reformulate this draft or use the following indications for you to craft the message: '{message_to_reformulate}'. "
-        else:
-            specific_instructions += "Provide a potential reply."
+        # Prepare the prompt for the language model
+        prompt = PromptTemplate(input_variables=[], template=template)
+        prompt_query = prompt.format()
 
-        # Combine common and specific instructions into the prompt
-        template = common_instructions + specific_instructions
-
-        prompt = PromptTemplate(
-            input_variables=["message_type", "tone"], template=template)
-        prompt_query = prompt.format(message_type=message_type, tone=tone)
-
-        # Run LLM model
+        # Generate the email response
+        progress_bar = st.progress(0)
         with st.spinner('Generating response...'):
             response = llm(prompt_query)
+        for i in range(100):
+            # Update progress bar
+            progress_bar.progress(i + 1)
+            # Pause for effect
+            time.sleep(0.01)
 
-        # Print results
-        st.write(response)
+        # Display the response
+        st.text_area("Email Response:", value=response, height=300)
     except OpenAIError as e:
         st.error(f"An OpenAI API error occurred: {str(e)}")
     except Exception as e:
         st.error(f"An unknown error occurred: {str(e)}")
 
 
+# Set up the form for user input
 with st.form("myform"):
-    original_message = st.text_area("Original Message to answer (optional):",
+    # Get the original message from the user
+    original_message = st.text_area("Email to answer:",
                                     help="Enter the original message that you want to reply to.")
-    message_to_reformulate = st.text_area(
-        "Message Draft to formulate:", help="Enter a draft of the message that you want to reformulate.")
+    # Get the key points from the user
+    key_points = st.text_area(
+        "Key points to include in the answer:", help="Enter the key points that should be included in the email response.")
+    # Add a submit button to the form
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
-    elif not original_message and not message_to_reformulate:
-        st.info("Please enter an original message or a message draft.")
+    elif not original_message:
+        st.info("Please enter an original message.")
+    # If the form is submitted, generate the email response
     elif submitted:
-        answer_message(original_message, message_to_reformulate)
+        answer_message(original_message, key_points)
